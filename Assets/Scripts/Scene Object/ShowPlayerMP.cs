e7636ada55cfb567368881f2244d0deabf793416
a15121212 <a14736920@gmail.com>
2021-06-01 21:52:39 +0800

    Start menu and +0 test

---
+++ Assets/Scripts/Scene Object/ShowPlayerMP.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class ShowPlayerMP : MonoBehaviour
+{
+   
+
+    private int MP = 100;
+    private Vector3 P2_position;
+    private GameObject P1RankUI;
+    private GameObject P2RankUI;
+    private Text myText;
+    private Text LackMPText;
+    private Color TextColor;
+    private int i; //to control the speed of adding MP
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myText = (GameObject.Find("MP_text")).GetComponent<Text>();
+        LackMPText = (GameObject.Find("lackMP")).GetComponent<Text>();
+        i = 0;
+        TextColor = LackMPText.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get current MP
+        int.TryParse(myText.text, out MP);
+
+        //add MP
+        i++; //to control the speed of adding MP
+        if ( MP < 100 && i >= 3)
+        {
+            MP++;
+            i = 0;
+        }
+
+        ////////////////////spell magic/////////////////
+        //magic 1
+       
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if (MP >= 30)
+            {
+                MP -= 30;
+                Debug.Log("法術1");
+
+            }
+            else
+            {
+                TextColor.a = 1;
+                LackMPText.color = TextColor;
+            }
+        }
+
+        //magic 2
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if (MP >= 30)
+            {
+                MP -= 30;
+                Debug.Log("法術2");
+
+            }
+            else
+            {
+                Debug.Log("MP不足");
+                TextColor.a = 1;
+                LackMPText.color = TextColor;
+            }
+
+        }
+
+        //magic 3
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            if (MP >= 25)
+            {
+                MP -= 25;
+                Debug.Log("法術3");
+
+            }
+            else
+            {
+                Debug.Log("MP不足");
+                TextColor.a = 1;
+                LackMPText.color = TextColor;
+            }
+
+        }
+
+        //magic 4
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            if (MP >= 75)
+            {
+                MP -= 75;
+                Debug.Log("法術4");
+
+            }
+            else
+            {
+                Debug.Log("MP不足");
+                TextColor.a = 1;
+                LackMPText.color = TextColor;
+            }
+
+        }
+
+        TextColor = LackMPText.color;
+        if( TextColor.a > 0)
+        {
+            TextColor.a -= 0.01f;
+            LackMPText.color = TextColor;
+        }
+
+
+        myText.text  = MP.ToString();
+    }
+}
---
+++ Assets/Scripts/Scene Object/ShowPlayerRank.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ShowPlayerRank : MonoBehaviour
+{
+    public GameObject Player1;
+    public GameObject Player2;
+
+    private Vector3 P1_position;
+    private Vector3 P2_position;
+    private Text P1Text;
+    private Text P2Text;
+
+    private Text temp;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+        P1Text = (GameObject.Find("Player1Rank")).GetComponent<Text>();
+
+        //  P2Text = GameObject.Find("Player2Rank");
+        P1Text.text = "1st";
+        // P2Text.text = "1st";
+
+    }
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        P1_position = Player1.transform.position;
+        P2_position = Player2.transform.position;
+        if( P1_position.z >= P2_position.z)
+        {
+            P1Text.text = "1st";
+            //P2Text.text = "2nd";
+        }
+        else
+        {
+            P1Text.text = "2nd";
+            //P2Text.text = "1st";
+
+        }
+
+    }
+}
